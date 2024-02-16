05404c502398564c465da4e38961d25bc7ff7098
Kirill <65077249+chk68@users.noreply.github.com>
2024-02-17 00:45:25 +0200

    Update PostRequest.php
    add request

--- laravel/app/Http/Requests/PostRequest.php
+++ laravel/app/Http/Requests/PostRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class PostRequest extends FormRequest
@@ -11,18 +12,25 @@ class PostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'content' => 'required|string',
+            'author' => 'required|string',
+            'pubDate' => 'required|date',
+            'thumbnailUrl' => 'required|url',
+            'link' => 'required|url',
+            'category' => 'required|string',
         ];
     }
 }
